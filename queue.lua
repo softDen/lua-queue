185b97b18ca50a601ca6c6104c937a4ea8dacfe0
JakubTetek <j.tetek@gmail.com>
2014-03-04 18:08:35 +0100

    refactoring

--- queue.lua
+++ queue.lua
@@ -1,5 +1,7 @@
 function getQueue()
-	local get = function(self)
+	queueObj = {}
+
+	queueObj.get = function(self)
 		if self["min"] >= self["max"] then
 			print("Tried get value from empty queue")
 			error()
@@ -8,11 +10,11 @@ function getQueue()
 		self["min"] = self["min"] + 1
 		return val
 	end
-	local put = function(self, val)
+	queueObj.put = function(self, val)
 		self["data"][self["max"]] = val
 		self["max"] = self["max"] + 1
 	end
-	local printQueue = function(self)
+	queueObj.printQueue = function(self)
 		io.write("\27[34m")
 		for x, e in pairs(self.data) do
 			if x == self.min then
@@ -24,13 +26,9 @@ function getQueue()
 		io.write("\27[0m\n")
 	end
 
-	queueObj = {}
 	queueObj["data"] = {}
 	queueObj["min"] = 1
 	queueObj["max"] = 1
-	queueObj["get"] = get
-	queueObj["put"] = put
-	queueObj["printQueue"] = printQueue
 
 	return queueObj
 end
